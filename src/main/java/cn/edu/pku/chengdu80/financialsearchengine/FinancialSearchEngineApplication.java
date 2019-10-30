e23f724e8847737b4684f42269aa432d322c5c96
David Xiang <xiangdongwei@pku.edu.cn>
2019-10-30 11:04:36 +0800

    init Spring Boot

---
+++ src/main/java/cn/edu/pku/chengdu80/financialsearchengine/FinancialSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.pku.chengdu80.financialsearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialSearchEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialSearchEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/pku/chengdu80/financialsearchengine/FinancialSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.pku.chengdu80.financialsearchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialSearchEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
